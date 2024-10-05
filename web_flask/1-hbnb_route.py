5e64a3a24e73bca1850e86312835a6187c0985a0
abdelghafour99 <abdelghafour83@live.fr>
2024-10-05 10:34:28 -0700

    File1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,10 +12,12 @@ def HBNB_hello():
     """display Hello HBNB!"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def HBNB():
     """display HBNB"""
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
